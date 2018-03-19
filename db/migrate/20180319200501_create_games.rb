c914db57b4063a21828bcbfa6239908c2ff46dcd
RSN-Bran <brandonsultana1@gmail.com>
2018-03-19 20:08:49 +0000

    New Migration

--- db/migrate/20180319200501_create_games.rb
+++ db/migrate/20180319200501_create_games.rb
@@ -1,12 +1,10 @@
 class CreateGames < ActiveRecord::Migration[5.1]
   def change
     create_table :games do |t|
-      t.string :gameid
       t.string :title
       t.string :coverart
 
       t.timestamps
     end
-    add_index :games, :gameid
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Game.create(title: "League of Legends", coverart: "/app/assets/images/league_of_legends.jpg")
+Game.create(title: "Overwatch", coverart: "/app/assets/images/overwatch.jpg")
+Game.create(title: "Super Smash Bros. Melee", coverart: "/app/assets/images/super_smash_bros_melee.jpg")
