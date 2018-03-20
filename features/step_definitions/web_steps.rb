214cc4109811e328c7f1f1de770bb30b46ac44ea
Mannan Kasliwal <mkasliw1@binghamton.edu>
2018-03-20 23:14:22 +0000

    Created Homepage controller, views, etc.; added web_steps.rb for future step definitions

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,12 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
