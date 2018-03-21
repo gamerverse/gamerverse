037d3d23d0042a81a1b3c08e73c7a649d29461e4
Mannan Kasliwal <mkasliw1@binghamton.edu>
2018-03-21 19:30:12 +0000

    Removed custom getters from Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,3 @@
 class Game < ApplicationRecord
-  def getCoverArtUrl
-    return coverart
-  end
-  
-  def getTitle
-    return title
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :games
+  
   get 'homepage/index'
 
   root 'homepage#index'
-
-  resources :games
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
