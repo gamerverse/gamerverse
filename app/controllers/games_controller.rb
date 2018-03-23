6a9f95b0ad6e9f4f53dd44746334b617816cc1d6
Mannan Kasliwal <mkasliw1@binghamton.edu>
2018-03-23 20:09:20 +0000

    Added games controller and view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
-    
-    
+  def index
+    # Store all of the games from the model in an instance variable so the view can access them
+    @games = Game.all
+  end
 end
